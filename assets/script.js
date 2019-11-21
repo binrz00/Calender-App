6580ed5b2927245ce36761e168af340f1ff06b67
Brian Zauner <binrz00@hotmail.com>
2019-11-20 22:27:58 -0600

    added the html with buttons that save to local storage.

---
+++ assets/script.js
@@ -0,0 +1,33 @@
+let display = moment().format("dddd, MMMM Do YYYY");
+let now = moment();
+/*how you set dates:
+let currentYear = moment().set('year', 2019);
+let currentMonth = moment().set('month', 11);  
+let currentDate = moment().set('date', 20);
+let currentHour = moment().set('hour', 20);
+let currentMinute = moment().set('minute', 20);*/
+//display the current day
+const displayDay = document.getElementById("currentDay");
+displayDay.innerHTML = display;
+//grab all the elements for future changes
+const container = document.getElementById("container");
+const buttons = document.querySelectorAll("button");
+//buttons have to save input in local storage
+buttons.forEach(function(button){
+button.addEventListener("click",function(){
+    localStorage.setItem("event","date of the event");
+})
+})
+
+
+//input fields (need to change color)
+const timeBlocks = document.querySelectorAll(".timeBlock-js");
+timeBlocks.forEach(function(item,index){
+//this will set their value to the time in 24 hour format
+item.setAttribute("id",index+=9)
+//if statements to change their class to match the time
+item.classList.add("future");
+
+
+
+})
