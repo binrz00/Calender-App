4e76f89e305eca267006b6c8ce62295f4e3df957
Brian Zauner <binrz00@hotmail.com>
2019-11-21 20:11:33 -0600

    I added local storage additions

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,5 @@
 let display = moment().format("dddd, MMMM Do YYYY");
 
-let now = moment();
-/*how you set dates:
-let currentYear = moment().set('year', 2019);
-let currentMonth = moment().set('month', 11);  
-let currentDate = moment().set('date', 20);
-let currentHour = moment().set('hour', 20);
-let currentMinute = moment().set('minute', 20);*/
 //display the current day
 const displayDay = document.getElementById("currentDay");
 displayDay.innerHTML = display;
@@ -14,13 +7,18 @@ displayDay.innerHTML = display;
 const container = document.getElementById("container");
 const buttons = document.querySelectorAll(".saveBtn");
 //these are the user inputs for the event
-const inputEls = document.querySelectorAll("textarea")
+const inputs = document.querySelectorAll("textarea")
 //buttons have to save input in local storage
-buttons.forEach(function (button) {
-    button.addEventListener("click", function () {
-        localStorage.setItem("", "date of the event");
+for(let i=0;i<buttons.length;i++){
+    buttons[i].addEventListener("click",function(){
+    const inputEl = inputs[i].value;
+    const prevData = localStorage.getItem("key"+i) || "[]";
+    const data = JSON.parse(prevData);
+    data.push(inputEl);
+    localStorage.setItem("key"+i, JSON.stringify(data));
     })
-})
+}
+
 //input fields (need to change color)
 const timeBlocks = document.querySelectorAll(".description");
 timeBlocks.forEach(function (item, index) {
@@ -29,18 +27,14 @@ timeBlocks.forEach(function (item, index) {
     //if statements to change their class to match the time
     if (index < JSON.parse(moment().format("H"))) {
         item.classList.add("past");
-        console.log(index);
-        console.log(JSON.parse(moment().format("H")));
     }
     if (index == JSON.parse(moment().format("H"))) {
-        console.log(index);
-        console.log(JSON.parse(moment().format("H")));
+
         item.classList.add("present");
     }
 
     if (index > JSON.parse(moment().format("H"))) {
         item.classList.add("future");
-        console.log(index);
-        console.log(JSON.parse(moment().format("H")));
+    
     }
 })
